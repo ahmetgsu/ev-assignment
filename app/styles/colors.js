da4fa3c7fa9b0b5b119b8b79dd901320dc05a757
ahmetgsu <ahmet.gsu@gmail.com>
2021-06-30 01:06:01 +0300

    Create colors object

---
+++ app/styles/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  main: '#2cc7e0',
+  white: '#fff',
+  black: '#000',
+  blackish: '#161b22',
+  black1: '#020408',
+  black2: '#121212',
+  black3: '#131c21',
+  gray1: '#262d31',
+};
