0a68e0e46a355030fc90b7d1689d0bf99756733f
ahmetgsu <ahmet.gsu@gmail.com>
2021-06-30 01:07:01 +0300

    Install new dependencies

--- app/App.js
+++ app/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import {View, Text, StyleSheet} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import {colors} from './styles/colors';
+import Logo from './assets/images/logo.svg';
+import {CHeader, Block} from './components/common';
 
 const App = () => {
-
   return (
-    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>EV Assignment</Text>
-    </View>
-
+    <Block flex={1} style={styles.container}>
+      <CHeader />
+      <Block flex={1}>
+        <Text>EV Assignment</Text>
+        <Logo height={35} width={186} />
+      </Block>
+    </Block>
   );
 };
 
 export default App;
 
-
 const styles = StyleSheet.create({
-
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.main,
+  },
 });
-
