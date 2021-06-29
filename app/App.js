7a619fe5ce5231bb47fca642ddd74e7e78a164dd
ahmetgsu <ahmet.gsu@gmail.com>
2021-06-29 10:46:50 +0300

    Create folder structure, Clean default boilerplates

---
+++ app/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>EV Assignment</Text>
+    </View>
+
+  );
+};
+
+export default App;
+
+
+const styles = StyleSheet.create({
+
+});
+
