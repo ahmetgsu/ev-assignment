b3f8cee1c3e2c85b893ff5c3a6359f249f332d04
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-03 15:27:22 +0300

    Create initial HomeScreenUser

---
+++ app/screens/HomeScreenUser.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet} from 'react-native';
+import {Block, Text} from '../components/common';
+import {colors} from '../styles/colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const HomeScreenUser = () => {
+  const [user, setUser] = useState();
+  console.log('HomeScreenUser -> user', user);
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const value = await AsyncStorage.getItem('user');
+    if (value !== null) {
+      console.log('HomeScreenUser -> value', value);
+      setUser(JSON.parse(value));
+    }
+  };
+  return (
+    <Block flex={1} center middle color={colors.white}>
+      <Text>User Home Screen</Text>
+    </Block>
+  );
+};
+
+export default HomeScreenUser;
+
+const styles = StyleSheet.create({});
