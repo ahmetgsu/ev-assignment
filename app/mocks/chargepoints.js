b7e8af8d37cabeef409ff7be614c0adc56398a5e
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-03 15:26:50 +0300

    Add mock chargepoints data

---
+++ app/mocks/chargepoints.js
@@ -0,0 +1,92 @@
+export const chargepoints = [
+  {
+    chargepointId: 1,
+    country: 'Germany',
+    operator: 'Stromnetz Hamburg',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.24,
+      transactionFee: 2.25,
+    },
+  },
+  {
+    chargepointId: 2,
+    country: 'Germany',
+    operator: 'NewMotion',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.48,
+      transactionFee: 2.45,
+    },
+  },
+  {
+    chargepointId: 3,
+    country: 'Germany',
+    operator: 'Aral',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.36,
+      transactionFee: 2.15,
+    },
+  },
+  {
+    chargepointId: 4,
+    country: 'Netherlands',
+    operator: 'EV-Box',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.34,
+      transactionFee: 2.25,
+    },
+  },
+  {
+    chargepointId: 5,
+    country: 'Netherlands',
+    operator: 'LastMileSolutions',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.68,
+      transactionFee: 2.45,
+    },
+  },
+  {
+    chargepointId: 6,
+    country: 'Netherlands',
+    operator: 'Vattenfall',
+    tariff: {
+      currency: 'EUR',
+      perkWH: 0.46,
+      transactionFee: 2.15,
+    },
+  },
+  {
+    chargepointId: 7,
+    country: 'United Kingdom',
+    operator: 'EV-Box',
+    tariff: {
+      currency: 'GBP',
+      perkWH: 0.14,
+      transactionFee: 1.25,
+    },
+  },
+  {
+    chargepointId: 8,
+    country: 'United Kingdom',
+    operator: 'LastMileSolutions',
+    tariff: {
+      currency: 'GBP',
+      perkWH: 0.54,
+      transactionFee: 3.15,
+    },
+  },
+  {
+    chargepointId: 9,
+    country: 'United Kingdom',
+    operator: 'Vattenfall',
+    tariff: {
+      currency: 'GBP',
+      perkWH: 0.16,
+      transactionFee: 1.15,
+    },
+  },
+];
