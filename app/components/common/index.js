4237a49a2de12ea4f79e9e6a0d2663385e09b1c7
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-03 15:25:52 +0300

    Add ColorButton to index file

--- app/components/common/index.js
+++ app/components/common/index.js
@@ -1,5 +1,6 @@
 import Block from './Block';
 import Text from './Text';
 import CHeader from './CHeader';
+import ColorButton from './ColorButton';
 
-export {Block, Text, CHeader};
+export {Block, Text, CHeader, ColorButton};
