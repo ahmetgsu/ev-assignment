4eb2ff9e03b26c2c63b9a196189934bd73211fd4
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-03 15:25:03 +0300

    Modify custom header component

--- app/components/common/CHeader.js
+++ app/components/common/CHeader.js
@@ -3,31 +3,38 @@ import {StyleSheet, TouchableOpacity} from 'react-native';
 import {Block, Text} from '.';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 import FastImage from 'react-native-fast-image';
+import {colors} from '../../styles/colors';
 
 const statusbarHeight = getStatusBarHeight();
 
-const CHeader = ({bgColor}) => {
+const CHeader = ({bgColor, title, back, right}) => {
   return (
     <Block
       flex={false}
       style={{...styles.header, backgroundColor: bgColor}}
       row>
       <Block flex={1} row center middle>
-        <TouchableOpacity>
-          <FastImage
-            source={require('../../assets/icons/left-chevron.png')}
-            resizeMode={FastImage.resizeMode.contain}
-            style={styles.backIcon}
-          />
-        </TouchableOpacity>
+        {back && (
+          <TouchableOpacity>
+            <FastImage
+              source={require('../../assets/icons/left-chevron.png')}
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.backIcon}
+            />
+          </TouchableOpacity>
+        )}
       </Block>
       <Block flex={3} row center middle>
-        <Text>center</Text>
+        <Text size={20} weight="bold">
+          {title}
+        </Text>
       </Block>
       <Block flex={1} row center middle>
-        <TouchableOpacity>
-          <Text>middle</Text>
-        </TouchableOpacity>
+        {right && (
+          <TouchableOpacity>
+            <Text>middle</Text>
+          </TouchableOpacity>
+        )}
       </Block>
     </Block>
   );
@@ -39,6 +46,8 @@ const styles = StyleSheet.create({
   header: {
     height: 60 + statusbarHeight,
     paddingTop: statusbarHeight,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray1,
   },
   backIcon: {
     width: 20,
