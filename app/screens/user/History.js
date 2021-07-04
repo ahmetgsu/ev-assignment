06cad4a1299c1adf387856bd10c4ec6a02c6c469
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-04 09:53:21 +0300

    Create initial History screen

---
+++ app/screens/user/History.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Block, Text} from '../../components/common';
+
+const History = () => {
+  return (
+    <Block flex={1} center middle>
+      <Text>History Screen</Text>
+    </Block>
+  );
+};
+
+export default History;
+
+const styles = StyleSheet.create({});
