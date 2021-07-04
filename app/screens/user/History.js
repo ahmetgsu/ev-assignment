a2956cf2c8d3501f0f61432c154d33843cc80c24
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-04 14:53:07 +0300

    Modify components

--- app/screens/user/History.js
+++ app/screens/user/History.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Block, Text} from '../../components/common';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+import {StyleSheet, FlatList} from 'react-native';
+import {Block, CHeader, Text} from '../../components/common';
+import {colors} from '../../styles/colors';
+import HistoryCard from '../../components/user/history/HistoryCard';
 
 const History = () => {
+  const isFocused = useIsFocused();
+  const [chargeHistory, setChargeHistory] = useState([]);
+
+  useEffect(() => {
+    isFocused && getUserChargeHistory();
+  }, [isFocused]);
+
+  const getUserChargeHistory = async () => {
+    const data = await AsyncStorage.getItem('charge-history');
+    if (data != null) {
+      const parsedData = JSON.parse(data);
+      setChargeHistory(parsedData);
+    }
+  };
+
+  const _renderItem = ({item, index}) => (
+    <HistoryCard item={item} index={index} />
+  );
   return (
-    <Block flex={1} center middle>
-      <Text>History Screen</Text>
+    <Block flex={1} color={colors.white}>
+      <CHeader title="Charge History" right />
+      <FlatList
+        data={chargeHistory}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={_renderItem}
+        style={styles.flatlist}
+      />
     </Block>
   );
 };
 
 export default History;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contentStyle: {
+    // paddingHorizontal: 25,
+    paddingBottom: 30,
+    flex: 1,
+  },
+  flatlist: {
+    paddingBottom: 10,
+    paddingTop: 20,
+    paddingHorizontal: 0,
+  },
+});
--- app/screens/user/Session.js
+++ app/screens/user/Session.js
@@ -2,28 +2,58 @@ import React, {useEffect, useState} from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 import {Block, CHeader, Text} from '../../components/common';
 import {colors} from '../../styles/colors';
-import SessionTracker from '../../components/user/SessionTracker';
+import SessionTracker from '../../components/user/session/SessionTracker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Operator from '../../components/user/Operator';
-import PricingDetails from '../../components/user/PricingDetails';
+import Operator from '../../components/user/session/Operator';
+import PricingDetails from '../../components/user/session/PricingDetails';
 import {Divider} from 'react-native-paper';
 import moment from 'moment';
+import BillingInfo from '../../components/user/session/BillingInfo';
+import {
+  calculateDiscount,
+  calculateEnergyConsumed,
+  calculateTotalBeforeDiscount,
+} from '../../helpers/billing-details';
+import {useIsFocused} from '@react-navigation/native';
 
 let currentTime;
 
 const Session = () => {
-  const [totalDuration, setTotalDuration] = useState(9000);
+  const isFocused = useIsFocused();
+  const [duration, setDuration] = useState();
+  const [energyConsumed, setEnergyConsumed] = useState();
+  const [totalBeforeDiscount, setTotalBeforeDiscount] = useState();
+  const [totalDiscount, setTotalDiscount] = useState();
+  const [totalToPay, setTotalToPay] = useState();
   const [stopwatchStart, setStopwatchStart] = useState(false);
   const [stopwatchReset, setStopwatchReset] = useState(false);
   const [startTime, setStartTime] = useState();
   console.log('Session -> startTime', startTime);
-  const [endtTime, setEndTime] = useState();
-  console.log('Session -> endtTime', endtTime);
+  const [endTime, setEndTime] = useState();
+  console.log('Session -> endTime', endTime);
   const [chargepoint, setChargePoint] = useState();
+  const [chargeHistory, setChargeHistory] = useState([]);
 
   useEffect(() => {
     getRelatedChargePoint();
+    getUserChargeHistory();
   }, []);
+
+  useEffect(() => {
+    endTime && calculateBillingDetails();
+  }, [endTime]);
+
+  useEffect(() => {
+    isFocused && setInitialStates();
+  }, [isFocused]);
+
+  const setInitialStates = () => {
+    setEnergyConsumed();
+    setTotalBeforeDiscount();
+    setTotalDiscount();
+    setTotalToPay();
+    setDuration();
+  };
 
   const getRelatedChargePoint = async () => {
     const data = await AsyncStorage.getItem('chargepoints');
@@ -35,39 +65,86 @@ const Session = () => {
       setChargePoint(relatedCP);
     }
   };
-
-  const getFormattedTime = time => {
-    console.log('Session -> time', time);
-    currentTime = time;
+  const getUserChargeHistory = async () => {
+    const data = await AsyncStorage.getItem('charge-history');
+    if (data != null) {
+      const parsedData = JSON.parse(data);
+      setChargeHistory(parsedData);
+    }
   };
 
-  const toggleStopwatch = () => setStopwatchStart(prev => !prev);
+  const toggleStopwatch = () => {
+    setStopwatchStart(prev => !prev);
+  };
 
   const startSession = () => {
-    setStartTime(moment());
+    setStartTime(moment().format());
     toggleStopwatch();
+    setStopwatchReset(false);
   };
   const stopSession = () => {
-    setEndTime(moment());
+    setEndTime(moment().format());
     toggleStopwatch();
+    setStopwatchReset(true);
   };
+
+  const calculateBillingDetails = () => {
+    const duration_ =
+      moment(endTime).format('ss') - moment(startTime).format('ss');
+    setDuration(duration_);
+    const {energy} = calculateEnergyConsumed(duration_, chargepoint.maxPower);
+    const {beforeDiscountTotal} = calculateTotalBeforeDiscount(
+      energy,
+      chargepoint.tariff,
+    );
+    const {combinedDiscount} = calculateDiscount(energy, chargepoint.tariff);
+    setTotalDiscount(combinedDiscount);
+    setEnergyConsumed(energy);
+    setTotalBeforeDiscount(beforeDiscountTotal);
+    const finalToPay = +(beforeDiscountTotal - combinedDiscount).toFixed(2);
+    setTotalToPay(finalToPay);
+    const newDataToStore = {
+      startTime: startTime,
+      endTime: endTime,
+      operator: chargepoint.operator,
+      country: chargepoint.country,
+      energyConsumed: energy,
+      chargeDuration: duration_,
+      totalBeforeDiscount: beforeDiscountTotal,
+      discount: combinedDiscount,
+      totalToPay: finalToPay,
+    };
+    console.log('calculateBillingDetails -> newDataToStore', newDataToStore);
+    const finalDataToStore = [newDataToStore, ...chargeHistory];
+
+    AsyncStorage.setItem('charge-history', JSON.stringify(finalDataToStore));
+    setStartTime();
+    setEndTime();
+  };
+
   return (
     <Block flex={1} color={colors.white}>
       <CHeader title="Charging Session" right />
-      <ScrollView style={styles.scrollview}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.scrollview}
+        contentContainerStyle={styles.content}>
         <Operator chargepoint={chargepoint} />
         <Divider theme={{color: colors.green1}} />
         <PricingDetails tariff={chargepoint?.tariff} />
         <Divider theme={{color: colors.green1}} />
-        <Block flex={false} pb={20} pt={20}>
-          <Text size={20}>Billing Information</Text>
-        </Block>
+        <BillingInfo
+          maxPower={chargepoint?.maxPower}
+          duration={duration}
+          energyConsumed={energyConsumed}
+          totalBeforeDiscount={totalBeforeDiscount}
+          totalDiscount={totalDiscount}
+          totalToPay={totalToPay}
+        />
       </ScrollView>
       <SessionTracker
         stopwatchStart={stopwatchStart}
         stopwatchReset={stopwatchReset}
-        getFormattedTime={getFormattedTime}
-        // toggleStopwatch={toggleStopwatch}
         start={startSession}
         stop={stopSession}
       />
@@ -82,6 +159,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 30,
     paddingTop: 20,
+  },
+  content: {
+    paddingBottom: 25,
   },
   divider: {
     color: colors.green1,
