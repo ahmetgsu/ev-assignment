859c0d43a7cf1f538e049817420aa1efb62ac2bf
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-06 00:31:16 +0300

    Add SplashScreen modification

--- app/App.js
+++ app/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import {useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {chargepoints} from './mocks/chargepoints';
 import AppStack from './navigation/AppNavigation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   useEffect(() => {
+    SplashScreen.hide();
     checkAndSetStorage();
   }, []);
 
