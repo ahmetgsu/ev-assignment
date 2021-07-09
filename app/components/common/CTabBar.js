39666ab625949e3cc222f3a150bddbfe172c2ede
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-09 23:28:41 +0300

    Create csutom tabbar component

---
+++ app/components/common/CTabBar.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import {colors} from '../../styles/colors';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Text from './Text';
+
+const CTabBar = ({state, descriptors, navigation}) => {
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={index.toString()}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingBottom: 15,
+            }}>
+            <FontAwesome5
+              name={
+                options.tabBarIcon(isFocused ? colors.main : colors.disabled)
+                  .props.name
+              }
+              color={isFocused ? colors.main : colors.disabled}
+              size={26}
+            />
+            <Text
+              size={18}
+              style={{color: isFocused ? colors.main : colors.disabled}}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 90,
+    backgroundColor: colors.white,
+    borderTopWidth: 1,
+    borderTopColor: colors.gray2,
+    shadowColor: colors.gray1,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+});
+
+export default CTabBar;
