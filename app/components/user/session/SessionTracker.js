cc23df0a90f0baa6360b9490d1876d0015915f4f
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-10 13:02:14 +0300

    Implement custom timer

--- app/components/user/session/SessionTracker.js
+++ app/components/user/session/SessionTracker.js
@@ -1,61 +1,13 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
-import {width} from '../../../helpers/device';
-import {colors} from '../../../styles/colors';
-import {Block, Text} from '../../common';
-import {Stopwatch} from 'react-native-stopwatch-timer';
+import {Block} from '../../common';
+import CTimer from '../../common/CTimer';
 
-const SessionTracker = ({
-  stopwatchStart,
-  stopwatchReset,
-  getFormattedTime,
-  start,
-  stop,
-}) => {
+const SessionTracker = ({start, stop}) => {
   return (
     <Block middle center flex={false} mb={20}>
-      <Stopwatch
-        laps
-        start={stopwatchStart}
-        reset={stopwatchReset}
-        options={options}
-        getTime={getFormattedTime}
-      />
-      <Block flex={false} mt={20}>
-        <TouchableOpacity onPress={!stopwatchStart ? start : stop}>
-          <Text
-            size={30}
-            color={!stopwatchStart ? colors.green1 : colors.red1}
-            style={styles.text}>
-            {!stopwatchStart ? 'Start' : 'Stop'}
-          </Text>
-        </TouchableOpacity>
-      </Block>
+      <CTimer start={start} stop={stop} />
     </Block>
   );
 };
 
 export default SessionTracker;
-
-const styles = StyleSheet.create({
-  text: {
-    letterSpacing: 3,
-  },
-});
-
-const options = {
-  container: {
-    backgroundColor: colors.green1,
-    padding: 10,
-    borderRadius: 8,
-    width: width - 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 35,
-    color: '#FFF',
-    // marginLeft: 7,
-    letterSpacing: 2,
-  },
-};
