dfb07afdc4ea683b6bf9642fbf0b66e9f7614edc
ahmetgsu <ahmet.gsu@gmail.com>
2021-07-10 13:00:38 +0300

    Create custom confirmation modal

---
+++ app/components/common/ConfirmationModal.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import Block from './Block';
+import Text from './Text';
+import Modal from 'react-native-modal';
+import {colors} from '../../styles/colors';
+
+const ConfirmationModal = ({isVisible, toggleModal, rightBtnAction}) => {
+  return (
+    <Modal
+      isVisible={isVisible}
+      onBackdropPress={toggleModal}
+      statusBarTranslucent
+      onRequestClose={toggleModal}
+      backdropColor="rgba(0,0,0,0.8)"
+      useNativeDriver
+      useNativeDriverForBackdrop
+      style={styles.modal}>
+      <Block flex={1} style={styles.container}>
+        <Block flex={2.5}>
+          <Block flex={1.5} row middle style={{alignItems: 'flex-end'}}>
+            <Text weight="600" style={{fontSize: 17}}>
+              Are you sure?
+            </Text>
+          </Block>
+          <Block flex={2} row middle style={styles.textBlock}>
+            <Text
+              center
+              style={{fontSize: 13, lineHeight: 16, letterSpacing: -0.08}}>
+              Do you really want to sign out?
+            </Text>
+          </Block>
+        </Block>
+        <Block row flex={1} center style={styles.buttonContainer}>
+          <Block flex={1}>
+            <TouchableOpacity onPress={toggleModal}>
+              <Block flex={false} row center middle style={styles.leftBtn}>
+                <Text color={colors.red1}>Cancel</Text>
+              </Block>
+            </TouchableOpacity>
+          </Block>
+          <Block flex={1}>
+            <TouchableOpacity onPress={rightBtnAction}>
+              <Block flex={false} row center middle style={styles.rightBtn}>
+                <Text color={colors.green1}>Confirm</Text>
+              </Block>
+            </TouchableOpacity>
+          </Block>
+        </Block>
+      </Block>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
+
+const styles = StyleSheet.create({
+  modal: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    backgroundColor: colors.white,
+    borderRadius: 14,
+    marginTop: 224,
+    height: 140,
+    marginHorizontal: 72,
+  },
+  container: {
+    backgroundColor: colors.white,
+    borderRadius: 20,
+  },
+  buttonContainer: {borderTopWidth: 1, borderColor: colors.gray2},
+  textBlock: {
+    paddingHorizontal: 16,
+    paddingTop: 15,
+  },
+  leftBtn: {
+    borderRightWidth: 1,
+    borderColor: colors.gray2,
+    height: '100%',
+  },
+  rightBtn: {
+    height: '100%',
+  },
+});
